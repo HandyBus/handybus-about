32e83599358979d4df2419dfe40554c4b7471594
jujeon <jwjsjwnsstjd@gmail.com>
2025-12-29 16:41:21 +0900

    feat: 핸드폰 양식 검증 추가

--- src/app/contact/components/types/contactForm.type.ts
+++ src/app/contact/components/types/contactForm.type.ts
@@ -3,7 +3,13 @@ import z from 'zod';
 export const contactFormSchema = z.object({
   name: z.string().min(1, '이름을 입력해주세요.'),
   company: z.string().min(1, '소속을 입력해주세요.'),
-  phoneNumber: z.string().min(1, '연락처를 입력해주세요.'),
+  phoneNumber: z
+    .string()
+    .min(1, '연락처를 입력해주세요.')
+    .regex(
+      /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/,
+      '올바른 연락처 형식이 아닙니다. (예: 010-1234-5678)',
+    ),
   email: z.email('올바른 이메일 형식이 아닙니다.'),
   title: z.string().min(1, '제목을 입력해주세요.'),
   content: z.string().min(1, '내용을 입력해주세요.'),
--- src/app/jobs/[postingId]/write/components/types/applicationForm.type.ts
+++ src/app/jobs/[postingId]/write/components/types/applicationForm.type.ts
@@ -6,7 +6,13 @@ export const ACCEPTED_FILE_TYPES = ['application/pdf'];
 export const applicationSchema = z
   .object({
     name: z.string().min(1, '이름을 입력해주세요.'),
-    contact: z.string().min(1, '연락처를 입력해주세요.'),
+    contact: z
+      .string()
+      .min(1, '연락처를 입력해주세요.')
+      .regex(
+        /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/,
+        '올바른 연락처 형식이 아닙니다. (예: 010-1234-5678)',
+      ),
     email: z
       .email('올바른 이메일 형식이 아닙니다.')
       .min(1, '이메일을 입력해주세요.'),
